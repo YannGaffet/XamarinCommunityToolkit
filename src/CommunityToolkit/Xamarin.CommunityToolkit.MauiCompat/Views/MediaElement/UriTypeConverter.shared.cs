c6a142d97a5b43b79d8454f7934c5eae96a708f5
YannGaffet <yann.gaffet@gmail.com>
2022-11-28 14:16:02 -0500

    MauiCompat

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit.MauiCompat/Views/MediaElement/UriTypeConverter.shared.cs
@@ -0,0 +1,15 @@
+﻿using System;using Microsoft.Extensions.Logging;
+using Microsoft.Maui; using Microsoft.Maui.Controls; using Microsoft.Maui.Graphics; using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Xamarin.CommunityToolkit.UI.Views
+{
+	[System.ComponentModel.TypeConverter(typeof(Uri))]
+	public class UriTypeConverter : System.ComponentModel.TypeConverter
+	{
+/*		public override object? ConvertFromInvariantString(string? value)
+		{
+			if (valueObject is not string value){throw new InvalidOperationException("Only typeof(string) allowed");}return string.IsNullOrWhiteSpace(value) ? null : new Uri(value, UriKind.RelativeOrAbsolute);
+		}*/
+	}
+}
